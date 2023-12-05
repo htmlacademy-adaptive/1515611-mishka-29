6e51953a75fc2d8cd8b7165c9f382e2088d5381f
Alexandra <cherkasash@gmail.com>
2023-12-05 19:50:58 +0300

    Corrects Form/Index mistakes

--- source/js/script.js
+++ source/js/script.js
@@ -3,4 +3,19 @@ const menuButton = document.querySelector(".header__nav-toggle");
 
 menuButton.addEventListener("click", () => {
   nav.classList.toggle("opened");
+  menuButton.classList.toggle("opened");
 });
+
+// Order modal
+const addToCartBtn = document.querySelector(".goods__button");
+const orderModal = document.querySelector("#order-modal");
+const addToCartModalBtn = document.querySelector("#add-to-cart-btn");
+
+addToCartBtn.addEventListener("click", () => {
+  orderModal.classList.add("show-modal");
+});
+
+addToCartModalBtn.addEventListener("click", () => {
+  orderModal.classList.remove("show-modal");
+});
+//
